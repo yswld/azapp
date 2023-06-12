ef1064554d27b8d89c22438e0d6f31982b69dc90
yswld <yyusukerad@gmail.com>
2023-06-12 15:29:06 +0900

    proxy

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.set('trust proxy', 1)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
